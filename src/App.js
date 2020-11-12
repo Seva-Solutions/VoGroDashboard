ac8bbb87c2e166e245bddc8a34806a98e5f0784c
Nandu <nandupokhrel@gmail.com>
2020-11-11 23:57:40 -0500

    sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import React from "react";
-
-import { Route, Switch } from "react-router-dom";
-
-import Header from "./components/Navbars/Header.jsx";
-import Sidebar from "./components/Sidebar/Sidebar.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-
-import routes from "./routes.js";
-
-class App extends React.Component {
+import React, { Component } from 'react';
+import MenuBar from './components/sidebar/MenuBar'
+import { BrowserRouter } from 'react-router-dom'
+import SideBar from './components/navbar/navbar'
+class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <Sidebar {...this.props} routes={routes}/>
-        <div className="main-panel">
-          <Header {...this.props} />
-          <Switch>
-            {routes.map((prop, key) => {
-              return (
-                <Route path={prop.layout + prop.path} component={prop.component} key={key} />
-              );
-            })}
-          </Switch>
-          <hr/>
-          <Footer />
-        </div>
-      </div>
-    );
+      <BrowserRouter>
+        {/* <MenuBar/> */}
+        <SideBar />
+      </BrowserRouter>
+    )
   }
 }
-
 export default App;
